5ff9b3d8892bda2f8cacab6f528d251fd3a2056f
daevv <dediaev.danil130702@gmail.com>
2023-06-15 17:19:10 +0300

    test 1

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,5 +1,5 @@
 export default () => {
-  const question = Math.floor(Math.random() * 10000);
+  const question = Math.floor(Math.random() * 100);
   const answer = (question % 2 === 0) ? 'yes' : 'no';
   return [question, answer];
 };
